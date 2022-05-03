ff66ee069c4cd67e0c52d7b1fab6a274051240f2
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-05-03 16:40:09 +0530

    feat: react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-
+import {Routes, Route} from 'react-router-dom';
+import { Category, Home, Login, Rules, Signup } from './Pages';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+   <Routes>
+     <Route path='/' element={<Home />}/>
+     <Route path='/category-page' element={<Category />} /> 
+     <Route path='/rules-page' element={<Rules />}/>
+     <Route path='/signup-page' element={<Signup />}/>
+     <Route path='/login-page' element={<Login />}/>
+
+   </Routes>
   );
 }
 
