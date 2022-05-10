1bb1cd7705662a2d05254c4b77b5a973b941bb42
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-05-10 11:58:37 +0530

    refactor:category card

--- src/Component/CategoryCard/CategoryCard.jsx
+++ src/Component/CategoryCard/CategoryCard.jsx
@@ -1,13 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './CategoryCard.css'
-const CategoryCard = ({ categoryImg, categoryName }) => {
+const CategoryCard = ({ categoryImg, categoryName, categoryId }) => {
+  const navigate = useNavigate()
+  const categoryCardHandler = (categoryId) => {
+    navigate('/rules-page')
+    sessionStorage.setItem('categoryId', categoryId)
+  }
+
   return (
-    <div className="card">
+    <div className="card" onClick={() => categoryCardHandler(categoryId)}>
       <Link to="/rules-page" className="text-link">
         <img src={categoryImg} alt="number img" className="category_img" />
         <div className="category_play_div">
           <p>{categoryName}</p>
-            <button className="btn number_play_btn">Play Now</button>
+          <button className="btn number_play_btn">Play Now</button>
         </div>
       </Link>
     </div>
