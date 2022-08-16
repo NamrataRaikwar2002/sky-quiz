c782a097b8651d5fdea129f552d761d233f86b0d
Namrata Raikwar <109650775+namrataRaikwar18@users.noreply.github.com>
2022-08-16 10:19:13 +0530

    added unit test for quiz reducer

---
+++ src/Pages/Question/Question.test.js
@@ -0,0 +1,63 @@
+
+import { quizReducer } from "../../hooks/Reducer/quizReducer";
+
+const initialState = {
+    rightAnsArr:[],
+    selectedOptions: []
+}
+
+describe("testing quiz reducer", () => {
+    test("properly add selected options", () => {
+        const action = {
+            type:"SLECTED_OPTION",
+            payload:[]
+        }
+        
+     const state = quizReducer(initialState, action)
+
+        const expectedState = {
+            rightAnsArr:[],
+            selectedOptions:[]
+        }
+
+        expect(state).toEqual(expectedState)
+    })
+
+    test("Currect answer", () => {
+
+        const action = {
+            type:"CURRECT_ANS",
+            payload:2
+        }
+        
+        
+     const state = quizReducer(initialState, action)
+
+        const expectedState = {
+            rightAnsArr:[2],
+            selectedOptions:[]
+        }
+
+        expect(state).toEqual(expectedState)
+
+    })
+
+    test('Reset when new quiz starts', () => {
+
+        const action = {
+            type:"RESET",
+            payload:[]
+        }
+
+        const state = quizReducer(initialState, action)
+
+        const expectedState = {
+            rightAnsArr:[],
+            selectedOptions: []
+        }
+
+        expect(state).toEqual(expectedState)
+
+    })
+
+})
